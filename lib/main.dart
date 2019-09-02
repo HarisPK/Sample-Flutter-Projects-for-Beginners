6ca9d1587601599c8f08b72e7557f03d7559f267
haris93 <haris@trymph.com>
2019-09-02 18:40:47 +0530

    following widgets are mentioned below: MaterialApp Scaffold AppBar Material

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Center(
-    child: Text(
-      "Text",
-      textDirection: TextDirection.ltr,
-    ),
-  ));
+  runApp(MaterialApp(
+      title: "SampleApp",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "SampleOneTitle",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(color: Colors.amber, fontSize: 20),
+          ),
+        ),
+        body: Material(
+          color: Colors.blue,
+          child: Center(
+            child: Text(
+              "Text",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(color: Colors.red, fontSize: 40),
+            ),
+          ),
+        ),
+      )));
 }

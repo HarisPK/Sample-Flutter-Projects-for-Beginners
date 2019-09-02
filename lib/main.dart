687b3baf52c0cacb58d5342416e4628beda11602
haris93 <haris93pk@gmail.com>
2019-09-02 22:57:49 +0530

    Organizing Code Using fat Arrow for single line

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(Center(
-    child: Text(
-      "Text",
-      textDirection: TextDirection.ltr,
-    ),
-  ));
+void main() => runApp(MyFlutterApp());
+
+class MyFlutterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,//dismiss debug banner
+      title: "My UI Design Class",
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Text(
+            "Class Title",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ),
+        body: Material(
+          color: Colors.amber,
+          child: Center(
+            child: Text(
+              "Wonderful",
+              style: TextStyle(color: Colors.red, fontSize: 30),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
